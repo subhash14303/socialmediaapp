ad4ed6dedafba9773dd88129e5122cc535244084
subhash14303 <pabbineedisubhash3@gmail.com>
2023-08-21 23:42:58 +0530

    social media project

---
+++ app/src/main/java/com/example/peeppalfinal/daos/PostDao.kt
@@ -0,0 +1,49 @@
+package com.example.peeppalfinal.daos
+
+import com.example.peeppalfinal.models.Post
+import com.example.peeppalfinal.models.User
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+class PostDao {
+    val db = FirebaseFirestore.getInstance()
+    val postCollections = db.collection("posts")
+    val auth = FirebaseAuth.getInstance()
+
+    fun addPost(text: String) {
+        GlobalScope.launch {
+            val currentUserId = auth.currentUser!!.uid
+            val userDao = UserDao()
+            val user = userDao.getUserById(currentUserId).await().toObject(User::class.java)!!
+
+            val currentTime = System.currentTimeMillis()
+            val post = Post(text, user, currentTime)
+            postCollections.document().set(post)
+        }
+    }
+    fun getPostById(postId: String): Task<DocumentSnapshot> {
+        return postCollections.document(postId).get()
+    }
+    fun updateLikes(postId: String) {
+        GlobalScope.launch {
+            val currentUserId = auth.currentUser!!.uid
+            val post = getPostById(postId).await().toObject(Post::class.java)!!
+            val isLiked = post.likedBy.contains(currentUserId)
+
+            if(isLiked) {
+                post.likedBy.remove(currentUserId)
+            } else {
+                post.likedBy.add(currentUserId)
+            }
+            postCollections.document(postId).set(post)
+        }
+
+    }
+}
